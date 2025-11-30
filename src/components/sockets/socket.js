950d96d4d90638ab2b528fb82b38fca26cc58462
SavantYash <yashsavant2004@gmail.com>
2025-12-01 01:19:25 +0530

    Update socket.js

--- src/components/sockets/socket.js
+++ src/components/sockets/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:4000";  
-
-export const socket = io(SOCKET_URL, {
+export const socket = io("https://chtbot-backend-production.up.railway.app", {
+  transports: ["websocket"],
+  secure: true,
   autoConnect: false
-});
+})
